8f88adaaa9cf922c8a4dd9b48210315005f42901
Olakunle Salami <kunsal2003@gmail.com>
2021-03-12 07:57:04 +0100

    installed native base

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Container, Text } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container style={styles.container}>
+      <Text>Open up App.js to start working on my application!</Text>
+    </Container>
   );
 }
 
